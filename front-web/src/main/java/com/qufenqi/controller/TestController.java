ce4713e813a6c224490cee3d71448ef563dcb63d
liuxin <liuxin@qudian.com>
2016-09-12 18:48:45 +0800

    提交哈哈

--- front-web/src/main/java/com/qufenqi/controller/TestController.java
+++ front-web/src/main/java/com/qufenqi/controller/TestController.java
@@ -2,18 +2,14 @@ package com.qufenqi.controller;/**
  * Created by zhangyang on 19/8/2016.
  */
 
-import com.qufenqi.controller.vo.TestVO;
-import com.qufenqi.edu.dao.po.Test;
+import com.qufenqi.controller.MyAnnotation.MonitorLog;
 import com.qufenqi.service.TestService;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 测试
@@ -22,20 +18,20 @@ import java.util.List;
  * @create 2016-08-19
  */
 @Controller
-public class TestController {
+public class TestController implements Ordered{
     @Autowired
     private TestService testService;
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @MonitorLog
     @ResponseBody
-    public List<TestVO> getData(){
-        List<Test> testList=testService.getAll();
-        if (CollectionUtils.isEmpty(testList)) return null;
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String getData(int i){
 
-        List<TestVO> testVOs=new ArrayList<TestVO>();
-        for (Test test:testList){
-            testVOs.add(new TestVO(test.getId(),test.getName()));
-        }
-        return testVOs;
+        return "10JQKA";
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
--- studentloan-service/src/test/java/com/qufenqi/AbstractTestBase.java
+++ studentloan-service/src/test/java/com/qufenqi/AbstractTestBase.java
@@ -1,7 +1,6 @@
 package com.qufenqi;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -9,5 +8,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:application-test.xml")
 public abstract class AbstractTestBase {
 
+    static String url = "http://123.56.104.144:10000/channel-server/daifu/payReal.do";
+    static String param = "";
+    static{
+        param = "{\n" +
+                "    \"text\" : \"5L8FKPy3b/cK4TDCmUmcaWpGrE6aywY30ZyscUtk7EFjF9WLGuY6j6d8JxdKG+XkwTWl96FuJr6DPzCIy/XyRSc6pxF2IcenbbVoww8CesKDLEMWq59tmLhLz8nNHAoz3LzNOta57CJMezrw/ETKQ02kpvjw3fs5QVLSa35wguAPb09p4ETFZExfUqM24rRCQb88PrsaPPphaD6EhXTgjBIuj7arYy/MXw46hDKs+nt1VqbDe8Gs4U0qpwpIOb3NnPoM5P7AqhTIKZQz9Ncm2z0+ZrVjmsjBhZgMJ3LKDE3VWyvawxSbTo+lSQT4oSXR0WVYp4Z51wiiLdZ6kxlUjgFtoXgrlMIe/0o6PHxT67zBa9bHDVYchSQQR1jFLQb8D+YCot6djHWTiJiOSRbWg9Xq5Rey1IISG459rFM0lTc=\n" +
+                "\",\n" +
+                "    \"bizCode\" : \"QUFENQI\",\n" +
+                "    \"sign\" : \"9b1afae1b962b344d87a50f17cdca965\"\n" +
+                "}";
+    }
 }
 
